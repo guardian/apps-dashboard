dc5e7e51ed091d5530b859392aa30b2977c4390c
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2016-04-13 14:59:31 +0100

    Add function to recursively fetch all reviews
    Very dirty implementation, but right now I'm interested in analysing
    the actual resulting data. To be refactored later if it proves
    valuable.

--- ios/AppAnnieClient.js
+++ ios/AppAnnieClient.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var _ = require('lodash');
 
 function AppAnnieClient(apiKey, androidProductID, iosProductID) {
 	module.auth = "Bearer " + apiKey;
@@ -57,6 +58,32 @@ AppAnnieClient.prototype.getAppStoreReviewsForVersionWithRating = function init(
 		} else {
 			callback(error);
 		}
+	});
+}
+
+var reviews = [];
+AppAnnieClient.prototype.getAppStoreReviewsForVersion = function (version, pageNum, callback) {
+	if(pageNum == 0)
+		reviews = [];
+	var options = {
+		url: 'https://api.appannie.com/v1.2/apps/ios/app/' + module.iosProductID + '/reviews?page_index=' + pageNum + '&version=' + version,
+		headers: {
+			'Accept': 'application/json',
+			'Authorization': module.auth
+		}
+	};
+	var that = this;
+
+	request(options, function(error, response, body) {
+		if (error || response.statusCode != 200)
+			callback(error);
+
+		var result = JSON.parse(body);
+		reviews = _.concat(reviews, result.reviews);
+		if(result.next_page)
+			that.getAppStoreReviewsForVersion(version, result.page_index + 1, callback)
+		else
+			callback(null, reviews);
 	});
 }
 
