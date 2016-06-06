5024f1270a40b986cc52117d982b864cd84ae454
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2016-06-06 11:51:44 +0100

    Remove credits from chart templates

--- android/Util.js
+++ android/Util.js
@@ -63,7 +63,10 @@ function Util() {
         }, {
             name: 'London',
             data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }]
+        }],
+        credits: {
+            enabled: false
+        },
     };
 
 	module["pie-basic"] = {
@@ -117,7 +120,10 @@ function Util() {
                 name: 'Proprietary or Undetectable',
                 y: 0.2
             }]
-        }]
+        }],
+        credits: {
+            enabled: false
+        },
     }
 
 	module["column-drilldown"] = {
@@ -370,7 +376,10 @@ function Util() {
                     ]
                 ]
             }]
-        }
+        },
+        credits: {
+            enabled: false
+        },
     }
 }
 
