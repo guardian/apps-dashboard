e6ce7c6b5f95db15723c6180fe7f904eacf491c8
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2016-06-10 09:41:06 +0100

    Bump version to 4.6

--- ios/crashRateWeek-render.js
+++ ios/crashRateWeek-render.js
@@ -39,7 +39,7 @@ function generateChart(callback) {
 		}
 
 		console.log('Crittercism API client initialized');
-		cc.errorGraph(["4f841104b0931561860003fc"],"crashPercent", 10080, {appVersion: "4.5 (13161)"}, function(err, result){
+		cc.errorGraph(["4f841104b0931561860003fc"],"crashPercent", 10080, {appVersion: "4.6 (13335)"}, function(err, result){
 			if (err) {
 				callback(err);
 			}
