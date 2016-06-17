6686c9f0d73faa2b9049a802da67a05b9ffbb14e
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2016-06-17 10:03:33 +0100

    Add version summary to crashes

--- android/crashSummary-render.js
+++ android/crashSummary-render.js
@@ -1,6 +1,7 @@
 var CrittercismClient = require('../lib/CrittercismClient.js');
 var Util = require('../lib/Util.js');
 var moment = require ('moment');
+var _ = require ('lodash');
 var nconf = require('nconf');
 var fs = require('fs');
 var GuardianApp = require('../lib/GuardianApp.js');
@@ -12,14 +13,15 @@ var clientid = nconf.get('crittercism_clientid');
 console.log('username is ' + username);
 console.log('clientid is ' + clientid);
 
-generateText(function(err, widespreadCrash, frequentCrash) {
+generateText(function(err, widespreadCrash, widespreadVersionSummary, frequentCrash, frequentVersionSummary) {
 	if(err) {
 		console.log(JSON.stringify(err.message))
 		throw err;
 	}
 
-	var js = `$("#widespreadCrash").html("Most widespread - <i>${widespreadCrash.name}</i> happening to <b>${widespreadCrash.uniqueSessionCount}</b> people.");`
-	js += `$("#frequentCrash").html("Most frequent - <i>${frequentCrash.name}</i>, with <b>${frequentCrash.sessionCount}</b> crashes so far.");`
+	var js = `$("#widespreadCrash").html("Most widespread - <i>${widespreadCrash.name}</i> happening to <b>${widespreadCrash.uniqueSessionCount}</b> people. ${widespreadVersionSummary}");
+$("#frequentCrash").html("Most frequent - <i>${frequentCrash.name}</i>, with <b>${frequentCrash.sessionCount}</b> crashes so far. ${frequentVersionSummary}");`
+
 	var filename = "crashSummary.js"
 	console.log(js);
 	fs.writeFile(filename, js, function(err) {
@@ -51,11 +53,58 @@ function generateText(callback) {
 
 			var widespreadCrash = mostWidespreadCrash(crashes);
 			var frequentCrash = mostFrequentCrash(crashes);
+			cc.getCrashDetails(widespreadCrash.hash, function(err, details) {
+				var diagnostic = details.diagnostics.discrete_diagnostic_data;
+				Util.print(diagnostic.system_version);
+				var widespreadVersionSummary = generateVersionSummary(diagnostic.system_version, diagnostic.app_version);
+				Util.print(widespreadVersionSummary);
+				cc.getCrashDetails(frequentCrash.hash, function(err, details) {
+					var diagnostic = details.diagnostics.discrete_diagnostic_data;
+					Util.print(diagnostic.system_version);
+					var frequentVersionSummary = generateVersionSummary(diagnostic.system_version, diagnostic.app_version);
+					Util.print(frequentVersionSummary);
+					callback(null, widespreadCrash, widespreadVersionSummary, frequentCrash, frequentVersionSummary);
+				});
+			});
 
-			callback(null, widespreadCrash, frequentCrash);
 		});
 	});
 };
+
+function generateVersionSummary(system_version, app_version) {
+	var androidVersion = crashOSVersionSummary(system_version)
+	var appVersion = crashAppVersionSummary(app_version)
+	Util.print(androidVersion);
+	Util.print(appVersion);
+	if(appVersion === "" && androidVersion === "")
+		return ""
+	else if (appVersion != "" && androidVersion != "")
+		return `Exclusive to <b>${appVersion}</b> and <b>${androidVersion}</b>.`
+	else if (appVersion != "" && androidVersion === "")
+		return `Exclusive to <b>${appVersion}</b>.`
+	else if (appVersion === "" && androidVersion !== "")
+		return `Exclusive to <b>${androidVersion}</b>.`
+
+}
+
+function crashOSVersionSummary(os) {
+	var versions = os.map(a => a[0]);
+	var majorVersions = _.uniq(versions.map(a => a.slice(0,9)));
+
+	if(majorVersions.length == 1)
+		return majorVersions[0].charAt(0).toUpperCase() + majorVersions[0].slice(1);
+	else
+		return "";
+}
+
+function crashAppVersionSummary(app) {
+	var versions = app.map(a => a[0]);
+
+	if(versions.length == 1)
+		return versions[0];
+	else
+		return "";
+}
 
 function mostWidespreadCrash(crashes) {
 	return crashes.sort((b,a) => a.uniqueSessionCount - b.uniqueSessionCount)[0];
