c0c7c69e50ee11d250a546ebf0de74e50a830bb8
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2016-09-20 11:22:58 +0100

    Add alpha to release table

--- release-table/releaseTable-render.js
+++ release-table/releaseTable-render.js
@@ -5,12 +5,25 @@ var Util = require('../lib/Util.js');
 
 
 var app = GuardianApp.getLatestAndroidApp();
+var beta = GuardianApp.getLatestAndroidBetaAsync();
+var alpha = GuardianApp.getLatestAndroidAlphaAsync();
 Util.print(app);
+Util.print(beta);
+Util.print(alpha);
 
 var js = `
 $('#productionReleased').text('${improveReadability(moment(app.releaseDate).from(moment()))}');
 $('#productionVersion').text('${app.version}');
 $('#productionDate').text('${Util.dayOfMonthAndWeek(app.releaseDate)}');
+
+$('#betaReleased').text('${improveReadability(moment(beta.releaseDate).from(moment()))}');
+$('#betaVersion').text('${beta.version}');
+$('#betaDate').text('${Util.dayOfMonthAndWeek(beta.releaseDate)}');
+
+$('#alphaReleased').text('${improveReadability(moment(alpha.releaseDate).from(moment()))}');
+$('#alphaVersion').text('${alpha.version}');
+$('#alphaDate').text('${Util.dayOfMonthAndWeek(alpha.releaseDate)}');
+
 `;
 console.log(js);
 
