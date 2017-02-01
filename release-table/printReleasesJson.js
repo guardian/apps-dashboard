56dbfd016e221d6c87e590e6c1fa795954eb00cd
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2017-02-01 16:25:07 +0000

    Add print releases json script

---
+++ release-table/printReleasesJson.js
@@ -0,0 +1,11 @@
+var moment = require ('moment');
+var _ = require('lodash');
+var GuardianApp = require('../lib/GuardianApp.js');
+
+var tracks = GuardianApp.getLatestGooglePlayReleases((err, releases) => {
+	var releasesExtendedDates = _.mapValues(releases, r => Object.assign(r, {
+		"releaseDateUnix": moment(r.releaseDate).unix(),
+		"releaseDateHumanReadable": moment(r.releaseDate).format('ddd MMM Do')}
+		));
+	console.log(JSON.stringify(releasesExtendedDates));
+});
