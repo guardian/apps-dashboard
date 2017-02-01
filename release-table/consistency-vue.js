b2ad980fb373f61bd83647c6c65d61696ceafc88
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2017-02-01 16:20:25 +0000

    Refactor dashboard to use vue.js

---
+++ release-table/consistency-vue.js
@@ -0,0 +1,28 @@
+window.onload=function(){
+var apiURL = 'releases.json'
+
+var demo = new Vue({
+
+  el: '#dataZone',
+
+  data: {
+    releases: null
+  },
+
+  created: function () {
+    this.fetchData()
+  },
+
+  methods: {
+    fetchData: function () {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', apiURL)
+      xhr.onload = () => {
+        this.releases = JSON.parse(xhr.responseText)
+      }
+      xhr.send()
+    }
+  }
+})
+
+}//]]> 
