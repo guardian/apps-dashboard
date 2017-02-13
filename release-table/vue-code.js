2429a13b7cf3439594f07e8bb37f6cb3d3684bb7
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2017-02-13 17:59:56 +0000

    Add support for staged rollout

--- release-table/vue-code.js
+++ release-table/vue-code.js
@@ -5,7 +5,7 @@ var demo = new Vue({
   el: '#dataZone',
 
   data: {
-    releases: {production:{version:"?",releaseDateUnix:0,releaseDateHumanReadable:"?"},"beta":{version:"?",releaseDateUnix:0,releaseDateHumanReadable:""},alpha:{version:"?",releaseDateUnix:0,releaseDateHumanReadable:"?"}}
+    releases: {production:{version:"?",releaseDateUnix:0,releaseDateHumanReadable:"?"},rollout:{version:"",releaseDateUnix:"",releaseDateHumanReadable:"",userFraction:""},"beta":{version:"?",releaseDateUnix:0,releaseDateHumanReadable:""},alpha:{version:"?",releaseDateUnix:0,releaseDateHumanReadable:"?"}}
   },
 
   created: function () {
@@ -18,7 +18,18 @@ var demo = new Vue({
       var xhr = new XMLHttpRequest()
       xhr.open('GET', apiURL)
       xhr.onload = () => {
-        this.releases = JSON.parse(xhr.responseText)
+	var response = JSON.parse(xhr.responseText)
+
+	if(typeof response.rollout === "undefined")
+		this.releases = response;
+	else
+		this.releases = Object.assign(this.releases, response)
+
+	this.releases.production.daysAgo = moment.unix(this.releases.beta.releaseDateUnix).diff(moment.now(), 'days') * -1 + 1;
+	this.releases.beta.daysAgo = moment.unix(this.releases.beta.releaseDateUnix).diff(moment.now(), 'days') * -1 + 1;
+	this.releases.alpha.daysAgo = moment.unix(this.releases.alpha.releaseDateUnix).diff(moment.now(), 'days') * -1 + 1;
+
+	console.log(this.releases);
       }
       xhr.send()
     }
