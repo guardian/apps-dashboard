7b04c2fc7d5ca0f5989add7522b051d684380c36
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2018-10-23 14:58:09 +0100

    Fix releases deployment

--- releases/boot.sh
+++ releases/boot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo Cmd click http://localhost:8000
+echo Access on network http://$(ifconfig | grep -o 10.249.[0-9]*.[0-9]* | head -1):8000
 /usr/bin/python -m SimpleHTTPServer 8000
